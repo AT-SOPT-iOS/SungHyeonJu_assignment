40bb750184100f5e2fe45ca94b132a096643bc10
hyeonju <shj040201@naver.com>
2025-04-24 04:09:36 +0900

    [#1] TextField Component 추가 + LoginVC 레이아웃 추가

--- TVING_Clone/TVING_Clone/Feature/LoginViewController.swift
+++ TVING_Clone/TVING_Clone/Feature/LoginViewController.swift
@@ -11,6 +11,19 @@ import SnapKit
 final class LoginViewController: BaseUIViewController {
 
     // MARK: - UI Components
+    private let idTextField: TextField = {
+        let textField = TextField()
+        textField.placeholder = "아이디"
+        textField.setPlaceholder(color: .gray2)
+        return textField
+    }()
+    
+    private let pwTextField: TextField = {
+        let textField = TextField()
+        textField.placeholder = "비밀번호"
+        textField.setPlaceholder(color: .gray2)
+        return textField
+    }()
 
     private let loginButton = BoxButton(title: "로그인하기", style: .outline)
 
@@ -41,13 +54,26 @@ final class LoginViewController: BaseUIViewController {
     // MARK: - UI Setup
 
     override func setUI() {
-        [loginButton, findAccountStack, signupStack]
+        [idTextField, pwTextField, loginButton, findAccountStack, signupStack]
             .forEach { view.addSubview($0) }
     }
 
     override func setLayout() {
+
+        idTextField.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(40)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.height.equalTo(52)
+        }
+
+        pwTextField.snp.makeConstraints {
+            $0.top.equalTo(idTextField.snp.bottom).offset(7)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.height.equalTo(52)
+        }
+        
         loginButton.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(80)
+            $0.top.equalTo(pwTextField.snp.bottom).offset(21)
             $0.horizontalEdges.equalToSuperview().inset(20)
             $0.height.equalTo(52)
         }
