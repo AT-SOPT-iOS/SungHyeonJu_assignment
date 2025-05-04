507f8d4bf6fe4dfd93743a241fed087520b7756c
hyeonju <shj040201@naver.com>
2025-05-04 19:32:40 +0900

    [#3] rootView 변경 익스텐션추가 및 적용

--- TVING_Clone/TVING_Clone/Global/Extension/UIView+.swift
+++ TVING_Clone/TVING_Clone/Global/Extension/UIView+.swift
@@ -12,4 +12,22 @@ extension UIView {
     func addSubviews(_ views: UIView...) {
         views.forEach { self.addSubview($0) }
     }
+
+    static func setRootViewController(_ viewController: UIViewController,
+                                      duration: TimeInterval = 0.4,
+                                      options: UIView.AnimationOptions = .transitionCrossDissolve) {
+        guard let window = UIApplication.shared.connectedScenes
+            .compactMap({ $0 as? UIWindowScene })
+            .first?.windows.first else {
+            return
+        }
+
+        UIView.transition(with: window,
+                          duration: duration,
+                          options: options,
+                          animations: {
+            window.rootViewController = viewController
+        },
+                          completion: nil)
+    }
 }
